4183b562ded3777f8285eaa9eba1f202131d9cd5
jeanvictorbr <jeanvictorbr@gmail.com>
2026-06-09 15:19:06 -0300

    Update ajuda.js

--- src/modules/economia/commands/ajuda.js
+++ src/modules/economia/commands/ajuda.js
@@ -10,13 +10,13 @@ export default {
             .setThumbnail(message.client.user.displayAvatarURL())
             .addFields(
                 { 
-                    name: '💼 TRABALHOS & CRIMES', 
-                    value: '`k trabalhar` - Escolha sua profissão.\n`k executar` - Trabalha e ganha dinheiro.\n`k batercarteira` - Roubo rápido de pedestres.\n`k roubar @user` - Assalta a carteira de um jogador.\n`k cd` - Veja seus tempos de descanso (cooldown).', 
+                    name: '💼 TRABALHOS, CRIMES & RPG', 
+                    value: '`k trabalhar` - Escolha sua profissão.\n`k executar` - Trabalha e ganha dinheiro.\n`k batercarteira` - Roubo rápido de pedestres.\n`k roubar @user` - Assalta a carteira de um jogador.\n`k habilidades` - Aprimore sua Sorte e Lábia na árvore de talentos.\n`k cd` - Veja seus tempos de descanso (cooldown).', 
                     inline: false 
                 },
                 { 
                     name: '🏦 GESTÃO FINANCEIRA', 
-                    value: '`k perfil` - Seu cartão de visitas, skills e saldo.\n`k dep [valor]` / `depall` - Guarda na conta bancária.\n`k sacar [valor]` - Tira dinheiro do banco.\n`k pix @user [valor]` - Transfere dinheiro eletronicamente.\n`k extrato` - Histórico das suas transações.\n`k rank` / `k top` - Os 10 magnatas mais ricos.', 
+                    value: '`k perfil` - Seu cartão de visitas, habilidades e saldo.\n`k dep [valor]` / `k depall` - Guarda na conta bancária.\n`k sacar [valor]` - Tira dinheiro do banco.\n`k pix @user [valor]` - Transfere dinheiro com comprovativo bancário.\n`k extrato` - Histórico oficial das suas transações.\n`k rank` / `k top` - Os 10 magnatas mais ricos.', 
                     inline: false 
                 },
                 { 
@@ -25,18 +25,18 @@ export default {
                     inline: false 
                 },
                 { 
+                    name: '🎰 CASSINO & APOSTAS', 
+                    value: '`k tigrinho` - Fortune Tiger Kibo HD! Rode as roletas e fature.\n`k mines [aposta] [minas]` - Encontre diamantes e fuja das bombas.\n`k crash [valor]` - Pare o multiplicador antes que o gráfico exploda.\n`k coinflip @user [valor]` - Aposte 50/50 na moeda contra outro jogador.', 
+                    inline: false 
+                },
+                { 
                     name: '🎁 RECOMPENSAS & VIP', 
                     value: '`k daily` - Seu bônus diário em dinheiro.\n`k mensal` - Bônus gigantesco a cada 30 dias.\n`k c` - Mostra seu cartão de Kibo Cash.', 
                     inline: false 
                 },
                 { 
-                    name: '🎰 CASSINO CLANDESTINO', 
-                    value: '`k crash [valor]` - Jogo de risco! Pare o multiplicador antes que exploda e multiplique seu dinheiro.', 
-                    inline: false 
-                },
-                { 
                     name: '🎭 INTERAÇÃO SOCIAL', 
-                    value: '`k abracar @user` - Dê um abraço apertado.\n`k bio` - Altere a msg do seu perfil.\n`k beijar @user` - Roube um beijo.\n`k socar @user` - Dê um direto na cara.\n`k chutar @user` - Um bico ninja.\n`k morder @user` - Mordida feroz.\n`k dancar @user` - Passinho sincronizado.', 
+                    value: '`k bio` - Altere a mensagem de status do seu perfil.\n`k abracar @user` - Dê um abraço apertado.\n`k beijar @user` - Roube um beijo.\n`k socar @user` - Dê um direto na cara.\n`k chutar @user` - Um bico ninja.\n`k morder @user` - Mordida feroz.\n`k dancar @user` - Passinho sincronizado.', 
                     inline: false 
                 }
             )
