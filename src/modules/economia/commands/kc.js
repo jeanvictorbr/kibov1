68682f8d954d93d1ae039625a16bec6fe2628b3e
jeanvictorbr <jeanvictorbr@gmail.com>
2026-06-09 00:46:39 -0300

    Update kc.js

--- src/modules/economia/commands/kc.js
+++ src/modules/economia/commands/kc.js
@@ -1,6 +1,7 @@
 import { AttachmentBuilder } from 'discord.js';
 import { prisma } from '../../../core/database.js';
-import { generateKcCanvas } from '../../../utils/canvasProfile.js'; // Ajuste o import se usar um arquivo novo
+// Corrigido o nome da função para bater com o que realmente existe no ficheiro
+import { generateProfileCanvas } from '../../../utils/canvasProfile.js'; 
 
 export default {
     name: 'kc',
@@ -11,7 +12,8 @@ export default {
             return message.reply('**Chefe, você ainda não tem um perfil registrado.** Tente `k perfil` primeiro.');
         }
 
-        const buffer = await generateKcCanvas(message.author, user);
+        // Usando a função correta
+        const buffer = await generateProfileCanvas(message.author, user);
         const attachment = new AttachmentBuilder(buffer, { name: 'kc_profile.png' });
 
         await message.channel.send({ files: [attachment] });
