b6936aea01163bbfa6f6cba80e9894a3e5970a3c
jeanvictorbr <jeanvictorbr@gmail.com>
2026-06-08 23:28:50 -0300

    SDA

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Client, GatewayIntentBits, Partials, Collection } from 'discord.js';
+import { execSync } from 'child_process';
 import { loadCommands, loadEvents, loadComponents } from './core/loader.js';
 import 'dotenv/config';
 
@@ -11,14 +12,8 @@ const client = new Client({
     ],
     partials: [Partials.Channel, Partials.Message],
     sweepers: {
-        messages: { 
-            interval: 3600, 
-            lifetime: 1800 
-        },
-        users: { 
-            interval: 3600,
-            filter: () => user => user.bot // Remove bots do cache para poupar RAM
-        }
+        messages: { interval: 3600, lifetime: 1800 },
+        users: { interval: 3600, filter: () => user => user.bot }
     }
 });
 
@@ -27,6 +22,14 @@ client.components = new Collection();
 
 (async () => {
     try {
+        // Sincroniza o banco de dados na hora de ligar
+        console.log("Sincronizando tabelas do PostgreSQL...");
+        try {
+            execSync("npx prisma db push --accept-data-loss", { stdio: 'inherit' });
+        } catch (dbError) {
+            console.error("Aviso no Banco de Dados (Verifique sua DATABASE_URL se os comandos falharem).");
+        }
+
         await loadCommands(client);
         await loadComponents(client);
         await loadEvents(client);
